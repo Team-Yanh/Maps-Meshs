aa7b9e06267f0cf0c73beb73d94dfcd8cdbb57be
timothee.denizou <timothee.denizou@epita.fr>
2021-03-05 18:43:38 +0100

    cleaned repo

--- display.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <SDL/SDL.h>
-#include "grid.h"
-
-size_t findOptimalCellSize(grid *g, SDL_Surface *surface);
-void drawGrid(grid *g, SDL_Surface *surface);
