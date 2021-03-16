1c3e271a8821220cfa91d37686657355cd043abf
hugo.hamon <hugo.hamon@epita.fr>
2021-03-16 17:34:03 +0100

    vector

---
+++ vector.c
@@ -0,0 +1,83 @@
+#include <err.h>
+#include "vector.h"
+
+struct vector *vector_new()
+{
+    struct vector *newvec = malloc(sizeof(struct vector));
+    if(!newvec)
+        errx(1,"Not enough memory");
+    newvec->data = calloc(1,sizeof(void *));
+    if(!(newvec->data))
+        errx(1,"Not enough memory!");
+    newvec->capacity = 1;
+    newvec->size = 0;
+    return newvec;
+}
+
+void vector_free(struct vector *v)
+{
+    free(v->data);
+    free(v);
+}
+
+void double_capacity(struct vector *v)
+{
+    v->data = realloc(v->data,v->capacity * sizeof(void *));
+    v->capacity *= 2;
+}
+
+void vector_push(struct vector *v, void *x)
+{
+    v->size ++;
+    if(v->size == v->capacity)
+        double_capacity(v);
+    *(v->data + v->size - 1) = x;
+}
+
+int vector_pop(struct vector *v, void **x)
+{
+    if(v->size == 0)
+        return 0;
+    v->size --;
+    *x = *(v->data + v->size);
+    *(v->data + v->size)= 0;
+    return 1;
+}
+
+int vector_get(struct vector *v, size_t i, void **x)
+{
+    if (i > v->size){
+        return 0;
+    }
+    *x = *(v->data + i - 1);
+    return 1; 
+}
+
+void vector_insert(struct vector *v, size_t i, void *x)
+{
+    if(i < 0 || i > v->size){
+        return ;
+    }
+    v->size ++;
+    if (v->size > v->capacity)
+        double_capacity(v);
+    void *temp;
+    for(size_t j = i - 1; j < v->size;j ++){
+        temp = *(v->data + j);
+        *(v->data + j) = x;
+        x = temp;
+    }
+}
+
+int vector_remove(struct vector *v, size_t i, void **x)
+{
+    if(i < 0 || i > v->size){
+        return 0;
+    }
+    v->size --;
+    *x = *(v->data+ i-1);
+    for(size_t j = i - 1; j < v->size;j ++){
+        *(v->data + j) = *(v->data +j+1);
+    }
+    return 1;
+}
