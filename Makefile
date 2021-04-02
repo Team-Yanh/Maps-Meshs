717f6d886bc3d62ad88ad837e1234be566cfa935
hugo.hamon <hugo.hamon@epita.fr>
2021-04-03 00:26:48 +0200

    modified main Makefile for compiling and vector no dereferencing

--- Makefile
+++ Makefile
@@ -1,14 +1,11 @@
 CC= gcc
 CFLAGS = -fsanitize=address -Wall -Wextra -g
-OGLFLAGS = -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl
-GTKFLAGS = `pkg-config --cflags gtk+-3.0` -Wall -O3 `pkg-config --libs gtk+-3.0`
-#GTKFLAGS = $(shell pkg-config --cflags gtk+-3.0)
-LDFLAGS = -static-libasan -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lGLU -lm
+LDFLAGS =  -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lGLU -lm
 LDLIBS = $(shell pkg-config --libs SDL_image) -lm
 
 FLAGS = $(CFLAGS) $(GTKFLAGS) $(LDLIBS) $(LDFLAGS)
 
-OBJ = vector.o queue.o map.o indices.o glad.o opengl.o CompleteLine.o colorPicker.o imageFilter.o imageUtils.o imageColoring.o display.o
+OBJ = vector.o queue.o CompleteLine.o imageFilter.o imageUtils.o imageColoring.o
 
 all: main
 
@@ -32,7 +29,6 @@ CompleteLine.o: vector.o CompleteLine.c CompleteLine.h
 imageFilter.o : imageFilter.h imageFilter.c
 imageUtils.o:  queue.o imageFilter.o imageUtils.h imageUtils.c 
 imageColoring.o:  queue.o imageFilter.o imageUtils.o imageColoring.h imageColoring.c
-display.o: imageFilter.o imageColoring.o imageUtils.o display.h display.c 
 
 .PHONY : clean
 
--- main.c
+++ main.c
@@ -3,38 +3,79 @@
 #include <err.h>
 #include <math.h>
 
-#include <gtk/gtk.h>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <GL/glut.h>
 #include <SDL/SDL.h>
 
 #include "SDL/SDL_image.h"
 #include "imageFilter.h"
 #include "imageUtils.h"
 #include "imageColoring.h"
-#include "colorPicker.h"
 #include "CompleteLine.h"
-#include "indices.h"
-#include "map.h"
-#include "opengl.h"
-#include "display.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    if(argc != 3)
-        err(EXIT_FAILURE, "Wrong number of argument: Requires only 2 (1 for the number of columns and one for the number of line)");
-    int col = atoi(argv[1]);
-    int line = atoi(argv[2]);
-
-    opengl_Create_Terrain(col, line);
-
     SDL_Init(SDL_INIT_VIDEO);
-    display_images();
-    display_images_hugo();
+    int formats = IMG_INIT_JPG | IMG_INIT_PNG;
+    int imageInit = IMG_Init(formats);
+    if((imageInit&formats) != formats)
+        errx(1, "Couldnt init SDL_Image");
+
+    SDL_Surface *image;
+
+    image = IMG_Load("images/test1.PNG");
+
+    if(image == NULL)
+        errx(1, "Couldnt load image");
+
+    SDL_Surface* screen = SDL_SetVideoMode(image->w, image->h, 32,
+            SDL_HWSURFACE | SDL_DOUBLEBUF);
+
+    SDL_Rect rcDest = {0, 0, 0, 0};
+
+    Color *black = initColor(image->format);
+    Color *white = initColor(image->format);
+    Color *red = initColor(image->format);
+    Color *green = initColor(image->format);
+    Color *orange = initColor(image->format);
+    setRGB(black, 0, 0, 0);
+    setRGB(white, 255, 255, 255);
+    setRGB(red, 255, 0, 0);
+    setRGB(green, 0, 255, 0);
+    setRGB(orange, 255, 165, 0);
+
+    //forEachPixel(image, cleanGray);
+
+    //forEachPixel(image, keepTopoLine);
+    //forEachPixel(image, keepTopoLineHSV);
+    //Uint32 topoColor = SDL_MapRGB(image->format, 203, 191, 171);
+    //Uint32 red = SDL_MapRGB(image->format, 255, 0, 0);
+
+
+    //int counter = colorZoneBFS(image, red, 550, 300);
+    //printf("counter from ColorZoneCount = %d\n", counter);
+    //colorCircles(image);
+    //replaceColor(image, green, black);
+
+    //colorAllZonesFromCircles(image);
+
+    //setMonochromatic(image, red);
+    //thickenColor(image, red);
+    FindAllExtremity(image);
+
+    SDL_BlitSurface(image, NULL, screen, &rcDest);
+    SDL_Flip(screen);
+    SDL_Delay(5000);
+    SDL_SaveBMP(image, "images/out.bmp");
+
+    freeColor(black);
+    freeColor(white);
+    freeColor(red);
+    freeColor(green);
+    freeColor(orange);
+
+    SDL_FreeSurface(image);
+    SDL_FreeSurface(screen);
     SDL_Quit();
 
-    colorPicker();
 
     return 0;
 }
