a7bc2d588ed9c1ef3580427038817de0e3b665f4
timothee.denizou <timothee.denizou@epita.fr>
2021-04-07 19:50:06 +0200

    added waitForKey SDL event in display.c

--- display.c
+++ display.c
@@ -1,5 +1,17 @@
 #include "display.h"
 
+void waitForClick()
+{
+    SDL_Event event;
+    SDL_bool click = SDL_FALSE;
+    while(!click)
+    {
+        SDL_WaitEvent(&event);
+        if(event.type == SDL_MOUSEBUTTONDOWN)
+            click = SDL_TRUE;
+    }
+}
+
 void display_images()
 {
     int formats = IMG_INIT_JPG | IMG_INIT_PNG;
@@ -9,7 +21,7 @@ void display_images()
 
     SDL_Surface *image;
 
-    image = IMG_Load("images/test5.png");
+    image = IMG_Load("images/test1.png");
 
     if(image == NULL)
         errx(1, "Couldnt load image");
@@ -30,14 +42,16 @@ void display_images()
     setRGB(green, 0, 255, 0);
     setRGB(orange, 255, 165, 0);
 
+    SDL_BlitSurface(image, NULL, screen, &rcDest);
+    SDL_Flip(screen);
+    waitForClick();
+    
     //forEachPixel(image, cleanGray);
 
     //forEachPixel(image, keepTopoLine);
     //forEachPixel(image, keepTopoLineHSV);
     //Uint32 topoColor = SDL_MapRGB(image->format, 203, 191, 171);
     //Uint32 red = SDL_MapRGB(image->format, 255, 0, 0);
-
-    thickenColor(image, black);
 
     //int counter = colorZoneBFS(image, red, 550, 300);
     //printf("counter from ColorZoneCount = %d\n", counter);
@@ -48,12 +62,16 @@ void display_images()
 
     //setMonochromatic(image, red);
     //thickenColor(image, red);
-    //printf("starting         \n");
-    //FindAllExtremity(image);
+
+    
 
     SDL_BlitSurface(image, NULL, screen, &rcDest);
     SDL_Flip(screen);
-    SDL_Delay(5000);
+
+    waitForClick();
+
+    thickenColor(image, black);
+
     SDL_SaveBMP(image, "images/out.bmp");
 
     freeColor(black);
@@ -65,6 +83,8 @@ void display_images()
     SDL_FreeSurface(image);
     SDL_FreeSurface(screen);
 }
+
+
 void display_images_hugo()
 {
     int formats = IMG_INIT_JPG | IMG_INIT_PNG;
@@ -84,11 +104,18 @@ void display_images_hugo()
 
     SDL_Rect rcDest = {0, 0, 0, 0};
 
+    SDL_BlitSurface(image, NULL, screen, &rcDest);
+    SDL_Flip(screen);
+
+    waitForClick();
+
     FindAllExtremity(image);
 
     SDL_BlitSurface(image, NULL, screen, &rcDest);
     SDL_Flip(screen);
-    SDL_Delay(5000);
+    
+    waitForClick();
+
     SDL_SaveBMP(image, "images/out.bmp");
 
     SDL_FreeSurface(image);
