109ee165b19ecc3c36a5ea52b0e4776dd266237d
Nicolas.CHARLES <nicolas.charles@epita.fr>
2021-04-22 11:30:38 +0200

    all camera movements

--- opengl.c
+++ opengl.c
@@ -10,6 +10,7 @@
 #include <math.h>
 #include "cglm/include/cglm/cglm.h"
 #include "shader.h"
+#include <stdbool.h>
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -18,6 +19,13 @@ vec3 cameraPos = {0.0f, 0.0f, 3.0f};
 vec3 cameraFront = {0.0f, 0.0f, -1.0f};
 vec3 cameraUp = {0.0f, 1.0f, 0.0f};
 
+bool firstMouse = true;
+float yaw = -90.0f;
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0f;
+float lastY = 600.0f / 2.0f;
+float fov = 45.0f;
+
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
@@ -25,9 +33,10 @@ const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "uniform mat4 view;\n"
     "uniform mat4 projection;\n"
+    "uniform mat4 model;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = projection * view * vec4(aPos, 1.0);\n"
+    "   gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
@@ -60,6 +69,10 @@ int opengl_Create_Terrain(int col, int line)
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -67,6 +80,8 @@ int opengl_Create_Terrain(int col, int line)
     {
         return -1;
     }
+    
+    glEnable(GL_DEPTH_TEST);
 
     //Shader ourShader;
     //ourShader.ID = new_Shader("shader/shader.vs", "shader/shader.fs");
@@ -130,8 +145,25 @@ int opengl_Create_Terrain(int col, int line)
     vertices[0] = -1.0f;
     vertices[1] = -1.0f;
     vertices[3] = (2.0f / col) - 1;
+   
+   
+    vertices[5] = 0.16f;
+    vertices[8] = 0.32f;
+    vertices[11] = 0.48f;
+    vertices[14] = 0.16f;
+    vertices[17] = 0.32f;
+    vertices[20] = 0.48f;
+    vertices[23] = 0.32f;
+    vertices[26] = 0.32f;
+    vertices[29] = 0.48f;
+    vertices[32] = 0.32f;
+    vertices[35] = 0.16f;
+    vertices[38] = 0.48f;
+    vertices[41] = 0.32f;
+    vertices[44] = 0.16f;
+    vertices[47] = 0.0f;
 
-    /*for(int i = 0; i < nb_vertices - 2; i+= 3)
+    for(int i = 0; i < nb_vertices - 2; i+= 3)
     {
         printf("Vertices --- i: %d, x: %f, y: %f, z: %f\n", i,  vertices[i], vertices[i + 1], vertices[i + 2]);
     }
@@ -139,7 +171,7 @@ int opengl_Create_Terrain(int col, int line)
     for(int i = 0; i < nb_indices - 2; i += 3)
     {
        printf("Indices --- i: %d, 1st: %u, 2nd: %u, 3rd; %u\n", i, indices[i], indices[i + 1], indices[i + 2]);
-    }*/
+    }
 
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -173,13 +205,6 @@ int opengl_Create_Terrain(int col, int line)
 
     glUseProgram(shaderProgram);
 
-    mat4 projection;
-    glm_mat4_identity(projection);
-    glm_perspective(glm_rad(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f, projection);
-    unsigned int perspLoc = glGetUniformLocation(shaderProgram, "projection");
-    glUniformMatrix4fv(perspLoc, 1, GL_FALSE, &projection[0][0]);
-    
-
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -192,64 +217,38 @@ int opengl_Create_Terrain(int col, int line)
         // -----
         processInput(window);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
         mat4 view;
-        //mat4 model;
-
-        //glm_mat4_identity(model);
+        mat4 model;
+        glm_mat4_identity(model);
         glm_mat4_identity(view);
 
-        /*float radius = 10.0f;
-        float camX = sin(glfwGetTime()) * radius;
-        float camZ = cos(glfwGetTime()) * radius;
-        vec3 eye = {camX, 0.0f, camZ};
-        vec3 dir = {0.0f, 0.0f, 0.0f};
-        vec3 up =  {0.0f, 1.0f, 0.0f};
-        */
         vec3 camera2;
+        vec3 rot = {1.0f, 0.0f, 0.0f};
+
+        //printf("CameraFront : %f, %f, %f \n", cameraFront[0], cameraFront[1], cameraFront[2]);
 
         glm_vec3_add(cameraPos, cameraFront, camera2);
 
         glm_lookat(cameraPos, camera2, cameraUp, view);
+        glm_rotate(model, glm_rad(-55.0f), rot);
 
-
-        //vec3 rot = {0.5f, 1.0f, 0.0f};
-        //vec3 trans = {0.0f, 0.0f, -3.0f};
-
-        //glm_rotate(model, (float)glfwGetTime(), rot);
-        //glm_translate(view, trans);
-
-        //unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
         unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
-
-        //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]);
+        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]);
 
+        mat4 projection;
+        glm_mat4_identity(projection);
+        glm_perspective(glm_rad(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f, projection);
+        unsigned int perspLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(perspLoc, 1, GL_FALSE, &projection[0][0]);
 
         float greenValue = 1;
         int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
         glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
-        /*mat4 model;
-        vec3 trans = {1.0f, 0.3f, 0.5f};
-        glm_mat4_identity(model);
-        glm_rotate(model, (float)glfwGetTime(), trans);
-        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]);
-        */
-
-        /*mat4 transform;
-        vec3 rot = {0.0f, 0.0f, 1.0f};
-        glm_mat4_identity(transform);
-        //glm_translate(transform, trans);
-        glm_rotate(transform, (float)glfwGetTime(), rot);
-
-        glUseProgram(shaderProgram);
-        unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (const float*)transform);
-        */
 
         //glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(VAO);
@@ -334,3 +333,49 @@ void setMat4(const char* name, mat4 mat, GLuint shaderProgram)
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram, name), 1, GL_FALSE, &mat[0][0]);
 }
 
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if(firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensivity = 0.1f;
+    xoffset *= sensivity;
+    yoffset *= sensivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if(pitch > 89.0f)
+        pitch = 89.0f;
+    if(pitch < -89.0f)
+        pitch = -89.0f;
+
+    vec3 front;
+    front[0] = cos(glm_rad(yaw)) * cos(glm_rad(pitch));
+    front[1] = sin(glm_rad(pitch));
+    front[2] = sin(glm_rad(yaw)) * cos(glm_rad(pitch));
+    
+    glm_vec3_normalize(front);
+
+    cameraFront[0] = front[0];
+    cameraFront[1] = front[1];
+    cameraFront[2] = front[2];
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    fov -= (float)yoffset;
+    if(fov < 1.0f)
+        fov = 1.0f;
+    if(fov > 45.0f)
+        fov = 45.0f;
+}
--- opengl.h
+++ opengl.h
@@ -6,5 +6,7 @@
 int opengl_Create_Terrain(int col, int line);
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 
 #endif
